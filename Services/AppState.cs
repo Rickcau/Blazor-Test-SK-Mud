f1a651195113b6bccec9986dccfb88cca002bd6e
Rick Caudle <rickcau@hotmail.com>
2024-05-08 20:48:31 -0400

    added tests for AI stuff for tomorrow

---
+++ Services/AppState.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Blazor_Test_SK_Mud.Services
+{ 
+       public class AppState
+        {
+
+            
+            public event Action? OnChange;
+
+            // Receive 'http' instance from DI
+           // private readonly HttpClient http;
+            //public AppState()
+            //{
+            //    // http = httpInstance;
+            //}
+
+            public void FilesUploaded()
+            {
+                NotifyStateChanged();
+            }
+            private void NotifyStateChanged() => OnChange?.Invoke();
+        }
+   
+}
